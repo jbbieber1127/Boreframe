c1105e2f2a392eb75dcbdf967542197954250944
jbbieber1127 <jbieber1127@gmail.com>
2017-08-16 20:34:45 -0400

    screencaps

--- src/ApplicationController.java
+++ src/ApplicationController.java
@@ -1,18 +1,20 @@
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javax.xml.xpath.XPath;
-import org.apache.commons.lang3.math.NumberUtils;
+import javafx.scene.image.ImageView;
+import javax.imageio.ImageIO;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -33,19 +35,9 @@ public class ApplicationController implements Initializable {
   private TextField searchBar;
   @FXML
   private Label valueLabel;
+  @FXML
+  private ImageView webview;
 
-  //  @Override
-//  public void initialize(URL location, ResourceBundle resources) {
-//    System.setProperty("webdriver.chrome.driver",
-//        "C:\\Users\\John\\IdeaProjects\\Warframe Price Checker\\out\\production\\Warframe Price Checker\\drivers\\chromedriver.exe");
-//
-//    driver = new ChromeDriver();
-//    driver.get("https://warframe.market");
-//
-//    searchElement = driver.findElement(By.xpath("//input[@id='search-item']"));
-//    buttonElement = driver.findElement(By.xpath(
-//        "/html[@class=' js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface no-generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths']/body[@class='site-warframe']/div[@class='wrapper']/div[@class='container w-wrapper']/div[@class='row'][3]/div[@class='col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 inner-blocks']/div[@class='search-item input-group']/span[@class='input-group-btn']/button[@class='btn btn-default']"));
-//  }
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 
@@ -59,8 +51,8 @@ public class ApplicationController implements Initializable {
     driver = new PhantomJSDriver(caps);
     driver.get("https://warframe.market");
 
-    searchElement = driver.findElement(By.xpath("//input[@id='search-item']"));
-    buttonElement = driver.findElement(By.cssSelector("span[class=input-group-btn]"));
+    searchElement = driver.findElementByCssSelector("input[id=search-item]");
+    buttonElement = driver.findElementByCssSelector("span[class=input-group-btn]");
 
   }
 
@@ -68,17 +60,23 @@ public class ApplicationController implements Initializable {
     searchElement.clear();
     searchElement.sendKeys(item);
     buttonElement.click();
+    File src = driver.getScreenshotAs(OutputType.FILE);
+
+    try {
+      webview.setImage(SwingFXUtils.toFXImage(ImageIO.read(src), null));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 
   private void evaluate() {
     int total = 0;
     int count = 0;
 
-//    WebElement elmt = driver.findElement(By.cssSelector("tbody[aria-live=polite]"));
-//    System.out.println("e" + elmt.getText());
-
-    List<WebElement> elements = driver.findElements(By.cssSelector("tr[role=row]"));
+    List<WebElement> elements = driver.findElementsByCssSelector("div[class=order-table]");
     System.out.println(elements.size());
+    System.out.println(elements.get(0).getText());
     for (int i = 0; i < elements.size(); i++) {
       String pricePath =
           "/html[@class=' js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface no-generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths']/body[@class='site-warframe']/div[@class='wrapper']/div[@class='container w-wrapper']/div[@class='row ']/div[@class='col-sm-12 inner-blocks']/div[@id='order-cont']/div[@id='sell']/div[@class='order-table']/table[@id='sell-table']/tbody/tr["
@@ -95,11 +93,6 @@ public class ApplicationController implements Initializable {
       count += num;
     }
     valueLabel.setText("Value: ~" + (count > 0 ? ((int) total / count) : 0) + " platinum");
-
-    List<WebElement> els = driver.findElements(By.xpath("//*[@class]"));
-    for(int i = 0; i < els.size(); i++){
-      System.out.println(els.get(i).getText());
-    }
   }
 
   @FXML
